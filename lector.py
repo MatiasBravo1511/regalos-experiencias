518a779da6ab35c52f655220df1f5a30b74dc389
MatiasBravo1511 <mjbravo4@uc.cl>
2025-05-08 00:38:11 -0400

    correccion de tiempo de espera

--- lector.py
+++ lector.py
@@ -7,4 +7,4 @@ from app import leer_emails_y_confirmar, confirmar_desde_correo
 while True:
     print("⏳ Revisando correos...")
     leer_emails_y_confirmar(confirmar_desde_correo)
-    time.sleep(60)  # cada 1 minuto
+    time.sleep(10)  # cada 10 segundos

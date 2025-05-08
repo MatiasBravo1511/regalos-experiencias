5d09d6d849e180f178e85823ec8bcfff81c4b440
MatiasBravo1511 <mjbravo4@uc.cl>
2025-05-08 00:21:17 -0400

    cambios en lector.py para que revise cada 1 minuto

--- lector.py
+++ lector.py
@@ -1,2 +1,7 @@
+import time
 from app import leer_emails_y_confirmar, confirmar_desde_correo
-leer_emails_y_confirmar(confirmar_desde_correo)
+
+while True:
+    print("⏳ Revisando correos...")
+    leer_emails_y_confirmar(confirmar_desde_correo)
+    time.sleep(60)  # cada 1 minuto

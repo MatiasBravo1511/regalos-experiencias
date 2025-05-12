79f95d3ea3ffe961c5d753b4b9db71e722367ff5
MatiasBravo1511 <mjbravo4@uc.cl>
2025-05-12 17:21:32 -0400

    cargar regalos dentro de while de lector

--- lector.py
+++ lector.py
@@ -1,7 +1,8 @@
-from app import leer_emails_y_confirmar, confirmar_desde_correo
+from app import leer_emails_y_confirmar, confirmar_desde_correo, cargar_regalos
 import time
 
 while True:
+    cargar_regalos()
     print("⏳ Revisando correos...", flush=True)
     leer_emails_y_confirmar(confirmar_desde_correo)
     time.sleep(10)  # cada 10 segundos

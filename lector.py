1c239ff747fa73f42847bda05db877768aeea88f
MatiasBravo1511 <mjbravo4@uc.cl>
2025-05-12 21:37:28 -0400

    prueba de logging para todos los prints

--- lector.py
+++ lector.py
@@ -1,6 +1,7 @@
 from app import leer_emails_y_confirmar, confirmar_desde_correo, cargar_regalos
 import time
 import logging
+import sys
 
 # Configura el logging para guardar en un archivo llamado "lector.log"
 logging.basicConfig(
@@ -10,10 +11,25 @@ logging.basicConfig(
     encoding="utf-8"
 )
 
+# Redirección de stdout y stderr a logging
+class StreamToLogger:
+    def __init__(self, logger, level):
+        self.logger = logger
+        self.level = level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.level, line.rstrip())
+
+    def flush(self):
+        pass
+
+sys.stdout = StreamToLogger(logging.getLogger("STDOUT"), logging.INFO)
+sys.stderr = StreamToLogger(logging.getLogger("STDERR"), logging.ERROR)
 
 while True:
     cargar_regalos()
     print("⏳ Revisando correos...", flush=True)
-    logging.info("⏳ Revisando correos...")
     leer_emails_y_confirmar(confirmar_desde_correo)
     time.sleep(10)  # cada 10 segundos

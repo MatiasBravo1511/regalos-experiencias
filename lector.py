32658a401a518685d878874f359465abe0f431a9
MatiasBravo1511 <mjbravo4@uc.cl>
2025-05-12 21:31:02 -0400

    prueba de logging en lector

--- lector.py
+++ lector.py
@@ -1,8 +1,19 @@
 from app import leer_emails_y_confirmar, confirmar_desde_correo, cargar_regalos
 import time
+import logging
+
+# Configura el logging para guardar en un archivo llamado "lector.log"
+logging.basicConfig(
+    filename="lector.log",
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    encoding="utf-8"
+)
+
 
 while True:
     cargar_regalos()
     print("⏳ Revisando correos...", flush=True)
+    logging.info("⏳ Revisando correos...")
     leer_emails_y_confirmar(confirmar_desde_correo)
     time.sleep(10)  # cada 10 segundos
